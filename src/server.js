88716184f3babb5b477aaef7f289ead8bde2d587
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-01 16:25:03 +0200

    change(helmet): add content security policy

--- src/server.js
+++ src/server.js
@@ -13,7 +13,23 @@ app.use(express.json())
 app.use(cors({
 	origin: ['http://localhost:5100', 'https://audity.dtpf.es']
 }))
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: [
+        "'self'",
+        "https://dev-yxiaxoiu73blg7k8.us.auth0.com",
+        "https://lh3.googleusercontent.com",
+      ],
+      objectSrc: ["'none'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "https: 'unsafe-inline'"],
+      connectSrc: ["'self'", "https://dev-yxiaxoiu73blg7k8.us.auth0.com/oauth/token"],
+      "img-src": ["'self'", "https: data:"],
+      upgradeInsecureRequests: [],
+    },
+  },
+}))
 app.use(`/api/${config.app.API_VERSION}`, userRoutes)
 app.use(`/api/${config.app.API_VERSION}`, genreRoutes)
 app.use(errorMiddleware)
