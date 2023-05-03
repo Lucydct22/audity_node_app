210be02a1fa95330554b7f3d59c57ce47ec464cd
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-03 09:20:25 +0200

    change(models): add new fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,12 +3,13 @@ const { Schema, model } = require('mongoose')
 const UserSchema = new Schema({
   userId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  lastname: { type: String },
+  lastname: String,
   nickname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  dateOfBirth: { type: Date },
-  country: { type: String },
-  language: { type: String },
+  dateOfBirth: Date,
+  country: String,
+  language: String,
+  role: String,
   following: [
     {
       type: Schema.Types.ObjectId,
