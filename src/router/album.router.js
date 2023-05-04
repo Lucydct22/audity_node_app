a1869253dbfb733c707275ad848c4144914204c1
Iuliia Shikhanova <yshihanova@gmail.com>
2023-05-04 17:14:35 +0100

    comment(album): album

--- src/router/album.router.js
+++ src/router/album.router.js
@@ -3,13 +3,13 @@ const album = require('../controllers/album.controllers')
 const md_auth = require('../middlewares/auth.middleware')
 const api = express.Router()
 
-api.get('/albums', album.getAlbums)
+/*api.get('/albums', album.getAlbums)
 api.get('/album/:albumId', album.getAlbumById)
 
 api.post('/albums', md_auth.ensureAuth, album.createAlbum)
 
 api.get('/albums/all', album.getAllAlbums)
 
-api.post('/albums/like', md_auth.ensureAuth, album.likeAlbum)
+api.post('/albums/like', md_auth.ensureAuth, album.likeAlbum)*/
 
 module.exports = api;
