3fd59a688daf6e83ad6d5dbae5d6aedb23efeee7
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-05 09:49:12 +0200

    feat(albumRouter): add getAlbumById route

--- src/router/album.router.js
+++ src/router/album.router.js
@@ -4,7 +4,7 @@ const md_auth = require('../middlewares/auth.middleware')
 const api = express.Router()
 
 api.get('/albums', album.getAlbums)
-// api.get('/album/:albumId', album.getAlbumById)
+api.get('/album/:albumId', album.getAlbumById)
 // api.post('/album', md_auth.ensureAuth, album.postAlbum)
 
 
