0d56d3209e71b49ae44dda02b049e50f6fed14ea
Iuliia Shikhanova <yshihanova@gmail.com>
2023-05-11 08:12:32 +0100

    update(delete): get:genre/:id -> controller

--- src/router/genre.router.js
+++ src/router/genre.router.js
@@ -5,7 +5,7 @@ const api = express.Router()
 
 api
   .get('/genres', controller.getGenres)
-  .get('/genre/:id', md_auth.checkJwt, controller.getGenreById)
+  .get('/genre/:id', controller.getGenreById)
   .post('/genre', md_auth.checkJwt, controller.postGenre)
   .put('/update-genre/:id', md_auth.checkJwt, controller.updateGenre)
   .delete('/delete-genre/:id', md_auth.checkJwt, controller.deleteGenre)
