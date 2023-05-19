7aee102c249813c1e47720490a1a88cd23d6c37e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-05-19 11:09:10 +0200

    feat: create route and controller to update total tracks played

--- src/router/statistic.router.js
+++ src/router/statistic.router.js
@@ -6,6 +6,7 @@ api
   .post('/statistic', [md_auth.ensureAuth, md_auth.ensureAdminAuth], controller.postStatistic)
   .put('/report-errored-track/:trackId', controller.reportErroredTrack)
   .put('/remove-report-errored-track/:trackId', [md_auth.ensureAuth, md_auth.ensureAdminAuth], controller.removeReportErroredTrack)
+  .put('/update-total-tracks-played', controller.updateTotalTracksPlayed)
   .get('/statistics', [md_auth.ensureAuth, md_auth.ensureAdminAuth], controller.getStatistics)
 
 module.exports = api;
